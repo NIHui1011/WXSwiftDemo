09160449b4d378520cc7ae5ec439a75783c312b9
huihui <416824366@qq.com>
2017-02-17 09:55:46 +0800

    new TabbarVC

--- WXSwiftDemo/AppDelegate.swift
+++ WXSwiftDemo/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let vc:TabbarViewController = TabbarViewController()
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = vc
+        
+        
         return true
     }
 
---
+++ WXSwiftDemo/TabbarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TabbarViewController.swift
+//  WXSwiftDemo
+//
+//  Created by 倪辉辉 on 2017/2/17.
+//  Copyright © 2017年 倪辉辉. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
